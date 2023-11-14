3807281df0c53e4d77c0f63c626f0e49498e64e0
Ehi <Ehi@gmail.com>
2023-11-14 10:24:58 +0000

    add bash shell script

---
+++ fileempty.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#This script is for file operators
+
+echo -n "Enter your name: "
+read name
+
+echo "Welcome "$name" "
+echo -e "Enter file name: \c"
+read filename
+read -p "What is your location: " loc
+if [ -s "$filename" ]
+   then
+        echo "There are content in the "$filename" file"
+   else
+        if [ -e "$filename" ];then
+	  echo "File is empty"
+	  else
+		echo  "file not found"
+	fi
+fi
+echo
+echo " I wish you a great stay at "$loc" "
